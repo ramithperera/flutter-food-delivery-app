9ad0e5101f6d3090bfa87aa2f3d950fadc2c13ab
Ramith <89182652+ramithperera@users.noreply.github.com>
2021-12-26 13:38:59 +0530

    halfway done

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -5,6 +5,7 @@ import 'package:food_delivery_app/screens/home/widget/food_list.dart';
 import 'package:food_delivery_app/screens/home/widget/food_list_view.dart';
 import 'package:food_delivery_app/screens/home/widget/resturant_info.dart';
 import 'package:food_delivery_app/widgets/custom_app_bar.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -45,8 +46,41 @@ class _HomePageState extends State<HomePage> {
               pageController,
               restaurant,
             ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            height: 60,
+            child: SmoothPageIndicator(
+              controller: pageController,
+              count: restaurant.menu.length,
+              effect: CustomizableEffect(
+                dotDecoration: DotDecoration(
+                  width: 8,
+                  height: 8,
+                  color: Colors.grey.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                activeDotDecoration: DotDecoration(
+                    width: 10,
+                    height: 10,
+                    color: kBackground,
+                    borderRadius: BorderRadius.circular(10),
+                    dotBorder: DotBorder(
+                      color: kPrimaryColor,
+                      padding: 2,
+                      width: 2,
+                    )),
+              ),
+              onDotClicked: (index) => pageController.jumpToPage(index),
+            ),
           )
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: kPrimaryColor,
+        elevation: 2,
+        child: Icon(Icons.shopping_bag_outlined, color: Colors.black, size: 30),
       ),
     );
   }
--- lib/screens/home/widget/food_list_view.dart
+++ lib/screens/home/widget/food_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
+import 'package:food_delivery_app/screens/home/widget/food_item.dart';
 
 class FoodListView extends StatelessWidget {
   final int selected;
@@ -13,7 +14,19 @@ class FoodListView extends StatelessWidget {
   Widget build(BuildContext context) {
     final category = restaurant.menu.keys.toList();
     return Container(
-      child: Text('ListView'),
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: PageView(
+        controller: pageController,
+        onPageChanged: (index) => callback(index),
+        children: category
+            .map((e) => ListView.separated(
+                padding: EdgeInsets.zero,
+                itemBuilder: (context, index) =>
+                    FoodItem(restaurant.menu[category[selected]]![index]),
+                separatorBuilder: (_, index) => SizedBox(height: 15),
+                itemCount: restaurant.menu[category[selected]]!.length))
+            .toList(),
+      ),
     );
   }
 }
